1809c74f36e680bd7c4a4feb5201a80b20b90ba7
Tanish <202301411@dau.ac.in>
2026-01-14 18:27:42 +0530

    small validation added

--- appointment-service/src/main/java/com/example/appointmentservice/dto/appointmentRequest.java
+++ appointment-service/src/main/java/com/example/appointmentservice/dto/appointmentRequest.java
@@ -1,8 +1,17 @@
 package com.example.appointmentservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class appointmentRequest {
+
+    @NotBlank(message = "Patient ID is required")
     private String patientId;
+
+    @NotBlank(message = "Doctor Name is required")
     private String doctorName;
+
+    @NotNull(message = "Appointment time is required")
     private String appointmentTime;
 
     public String getPatientId() {
